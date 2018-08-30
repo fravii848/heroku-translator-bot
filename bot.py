507d73ea76bed10eb040aedd3a430f35bd63dd36
Jaki99k <33793346+Jaki99k@users.noreply.github.com>
2018-08-30 11:48:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,57 +1,51 @@
-#Data creazione 21/08/2018
-# coding=utf-8
-
 import telepot
-import subprocess
-import speech_recognition as sr
-from pydub import AudioSegment
+from googletrans import Translator
+from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
-recognizer_instance = sr.Recognizer()
+translator = Translator()
+origine = "it"
+dest = "en"
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
-    if content_type == 'voice':
-        name = msg["from"]["first_name"]
-        lon = msg["voice"]["duration"]
-        voiceName = msg["voice"]["file_id"]
-        mimeType = msg["voice"]["mime_type"]
-        bot.download_file(msg["voice"]["file_id"], './audio.ogg')
-        subprocess.call(['ffmpeg', '-v', 'quiet', '-y', '-i', 'audio.ogg', 'audio.wav'])
-        wav =  sr.AudioFile('audio.wav')
-        with wav as source:
-            recognizer_instance.pause_threshold = 3.0
-            audio = recognizer_instance.listen(source)
-            print('Elaborazione messaggio in corso...')
-            bot.sendMessage(chat_id, "Elaborazione...")
 
-        try:
-            text = recognizer_instance.recognize_google(audio, language="it-IT")
-            print("Sono riuscito a comprendere: \n", text)
-            bot.sendMessage(chat_id, "Messaggio: %s\n"%text)
-        except Exception as e:
-            print(e)
+    if content_type == 'text':
+        if msg['text'] == '/dt': #destination
+            destinazione = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text="English🇺🇸", callback_data="enDest")],[InlineKeyboardButton(text="Italiano🇮🇹", callback_data="itDest")], [InlineKeyboardButton(text="French🇫🇷", callback_data="frDest")]])
 
-        bot.sendMessage(chat_id, "Il messaggio inviato è di tipo vocale!")
-        print('Utente: %s'%name)
-        if lon > 1:
-            print('Lunghezza audio: %d'%lon,'secondi')
+            bot.sendMessage(chat_id, "Scegli la lingua di destinazione", reply_markup=destinazione)
+        elif msg['text'] == '/sr': #source
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                [InlineKeyboardButton(text='English🇺🇸', callback_data='en')],[InlineKeyboardButton(text="Italiano🇮🇹", callback_data="it",)], [InlineKeyboardButton(text="French🇫🇷", callback_data="fr")]])
+
+            bot.sendMessage(chat_id, "Scegli la lingua di origine", reply_markup=keyboard)
         else:
-            print('Lunghezza audio: %d'%lon,'secondo')
+            txt = msg["text"]
+            print("Origine:",origine,"Destinazione:",dest)
+            translations = translator.translate(txt, src=origine, dest=dest)
+            bot.sendMessage(chat_id, translations.text)
+            name = msg["from"]["first_name"]
+            print(name, msg["text"], " -> ", translations.text)
 
-TOKEN = '573952513:AAHp0U1TP_zBo4b-I2EVk_Sgk4EUPp8cNaA'
+def on_callback_query(msg):
+    global origine
+    global dest
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    print("Query data impostato su : ", query_data)
 
+    if len(query_data) > 2:
+        dest = query_data[0:2]
+        bot.answerCallbackQuery(query_id, text=query_data + "Impostato come lingua di destinazione")
+    else:
+        origine = query_data
+        bot.answerCallbackQuery(query_id, text=query_data + "Impostato come lingua di origine")
+
+#Creazione del BOT
+TOKEN = "678974937:AAHUY2TytUeUJx4w7mEppJ9pgu2FMZ94i6g"
 bot = telepot.Bot(TOKEN)
-bot.message_loop(on_chat_message)
-
-print('Listening for inputs... ')
+bot.message_loop({'chat': on_chat_message, 'callback_query': on_callback_query})
 
 import time
-
 while 1:
     time.sleep(10)
-
-
-#DA RICORDARE
-
-#bot.sendMessage(chat_id, 'ciao %s, sono un bot molto stupido!'%name)
-#bot.sendMessage(chat_id, 'ho ricevuto questo : %s'%txt)
